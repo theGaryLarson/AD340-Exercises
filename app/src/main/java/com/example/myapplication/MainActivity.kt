888c64631ac84bd3ecff4a03209059031657e61d
theGaryLarson <gary.larson316@gmail.com>
2023-04-19 01:09:42 -0700

    project files

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,24 +2,36 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ourTextView: TextView = findViewById(R.id.textView)
-        val button: TextView = findViewById(R.id.button1)
-        val text = "Test message"
-        val duration = Toast.LENGTH_LONG
-        button.text = "Click Me!"
-        val toast = Toast.makeText(applicationContext, text, duration)
-        button.setOnClickListener {
+        val food: Button = findViewById(R.id.food)
+        val touch: Button = findViewById(R.id.touch)
+        val image: ImageView = findViewById(R.id.imageView)
+
+        var message = ""
+        val duration = Toast.LENGTH_SHORT
+        food.text = "Food"
+        touch.text = "Touch"
+        val toast = Toast.makeText(applicationContext, message, duration)
+
+        food.setOnClickListener {
+            toast.setText("Good Person leave my food alone -,-")
+            image.setImageResource(R.drawable.shiba_calm)
             toast.show()
         }
 
-
+        touch.setOnClickListener {
+            toast.setText("MINE! MINE! MINE! MINE!")
+            image.setImageResource(R.drawable.angry_shiba)
+            toast.show()
+        }
     }
 }
